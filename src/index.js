02ac6e240ca2228ba5012646d10b2e22bbe76b03
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-08 20:51:56 +0100

    adding map to component

--- src/index.js
+++ src/index.js
@@ -3,26 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
+
 class Card extends React.Component {
   render() {
-    return ( 
-      <div className="github-profile">
-        <img src="https://res.cloudinary.com/stealthman22/image/upload/v1586308024/new-portfolio/hero/time-lapse-photography-of-waterfalls-during-sunset-210186.jpg" alt="null" />
-        <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company Here...</div>
+    return this.props.data.map(profile => {
+      return (
+        <div className="github-profile">
+          <img src={profile.avatar_url} alt="null" />
+          <div className="info">
+            <div className="name">{profile.name}.</div>
+            <div className="company">{profile.company}</div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    })
   }
 }
+
+const CardList = (props) => (
+  <div>
+    <Card data={testData}/>
+  </div>
+)
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </div>
     );
   }
